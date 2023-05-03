ce9518f29df5b6f0fd0a99986d17d75e7e85809e
schmeb0704 <seanauditor0704@gmail.com>
2023-05-03 14:20:00 +0800

    initial commits for recipe book

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss'],
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test description',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Recipe(
+      'Test Recipe 2',
+      'Test description 2',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+  ];
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.scss'],
+})
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Onion', 5),
+    new Ingredient('Tomato', 10),
+  ];
+}
